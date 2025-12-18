77d0b93eef529c01f2d9296a93d18e8a06df23f3
bala-amypo <bala@amypo.in>
2025-12-18 14:51:12 +0000

    User commit

--- src/main/java/com/example/demo/service/SupplyForecastService.java
+++ src/main/java/com/example/demo/service/SupplyForecastService.java
@@ -2,17 +2,16 @@ package com.example.demo.service;
 
 import java.util.List;
 import java.util.Optional;
+
 import com.example.demo.entity.SupplyForecast;
 
 public interface SupplyForecastService {
 
-    SupplyForecast createForecast(SupplyForecast forecast);
+    SupplyForecast save(SupplyForecast forecast);
 
-    SupplyForecast updateForecast(Long id, SupplyForecast forecast);
+    Optional<SupplyForecast> getById(Long id);
 
-    Optional<SupplyForecast> getForecastById(Long id);
+    SupplyForecast getLatest();
 
-    SupplyForecast getLatestForecast();
-
-    List<SupplyForecast> getAllForecasts();
+    List<SupplyForecast> getAll();
 }
