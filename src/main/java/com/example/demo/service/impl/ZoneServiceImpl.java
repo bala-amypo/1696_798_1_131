28233f58a07ebda9bed0d699d54be9baf4e45627
bala-amypo <bala@amypo.in>
2025-12-18 15:31:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
@@ -1,14 +1,12 @@
 package com.example.demo.service.impl;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Zone;
 import com.example.demo.repository.ZoneRepository;
 import com.example.demo.service.ZoneService;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ZoneServiceImpl implements ZoneService {
@@ -17,34 +15,35 @@ public class ZoneServiceImpl implements ZoneService {
     private ZoneRepository repo;
 
     @Override
-    public Zone save(Zone zone) {
+    public Zone createZone(Zone zone) {
         return repo.save(zone);
     }
 
     @Override
-    public Zone update(Long id, Zone zone) {
+    public Zone updateZone(Long id, Zone zone) {
         zone.setId(id);
         return repo.save(zone);
     }
 
     @Override
-    public Optional<Zone> getById(Long id) {
+    public Optional<Zone> getZoneById(Long id) {
         return repo.findById(id);
     }
 
     @Override
-    public List<Zone> getAll() {
+    public List<Zone> getAllZones() {
         return repo.findAll();
     }
 
     @Override
-    public String deactivate(Long id) {
-        Optional<Zone> zone = repo.findById(id);
-        if (zone.isPresent()) {
-            zone.get().setActive(false);
-            repo.save(zone.get());
-            return "Zone Deactivated";
+    public String deactivateZone(Long id) {
+        Optional<Zone> optionalZone = repo.findById(id);
+        if(optionalZone.isPresent()){
+            Zone zone = optionalZone.get();
+            zone.setActive(false);
+            repo.save(zone);
+            return "Zone deactivated successfully";
         }
-        return "Zone Not Found";
+        return "Zone not found";
     }
 }
