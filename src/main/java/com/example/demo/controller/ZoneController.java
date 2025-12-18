95f7ac9dd76dea9b83304c82a0b26c0951622e35
bala-amypo <bala@amypo.in>
2025-12-18 15:15:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/ZoneController.java
+++ src/main/java/com/example/demo/controller/ZoneController.java
@@ -1,22 +1,5 @@
-package com.example.demo.controller;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import com.example.demo.entity.Zone;
-import com.example.demo.service.ZoneService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/zones")
-@Tag(name = "Zone Controller")
 public class ZoneController {
 
     @Autowired
@@ -24,27 +7,26 @@ public class ZoneController {
 
     @PostMapping("/")
     public Zone create(@RequestBody Zone zone) {
-        return zoneService.createZone(zone);
+        return zoneService.save(zone);
     }
 
     @PutMapping("/{id}")
     public Zone update(@PathVariable Long id, @RequestBody Zone zone) {
-        return zoneService.updateZone(id, zone);
+        return zoneService.update(id, zone);
     }
 
     @GetMapping("/{id}")
     public Optional<Zone> getById(@PathVariable Long id) {
-        return zoneService.getZoneById(id);
+        return zoneService.getById(id);
     }
 
     @GetMapping("/")
     public List<Zone> getAll() {
-        return zoneService.getAllZones();
+        return zoneService.getAll();
     }
 
-    @PostMapping("/deactivate/{id}")
-    public ResponseEntity<String> deactivate(@PathVariable Long id) {
-        String result = zoneService.deactivateZone(id);
-        return ResponseEntity.ok(result);
+    @PostMapping("/{id}/deactivate")
+    public String deactivate(@PathVariable Long id) {
+        return zoneService.deactivate(id);
     }
 }
