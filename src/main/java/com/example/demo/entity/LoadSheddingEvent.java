33f598cfa228d811c62c045821ae9625926aae65
bala-amypo <bala@amypo.in>
2025-12-19 05:53:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoadSheddingEvent.java
+++ src/main/java/com/example/demo/entity/LoadSheddingEvent.java
@@ -1,42 +1,5 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-
-import java.time.Instant;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class LoadSheddingEvent {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    private Zone zone;
-
-    private Instant eventStart;
-    private Instant eventEnd;
-    private String reason;
-    private Long triggeredByForecastId;
-    private Double expectedDemandReductionMW;
-}
-package com.example.demo.entity;
-
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -58,6 +21,7 @@ public class LoadSheddingEvent {
     private Zone zone;
 
     private Instant eventStart;
+
     private Instant eventEnd;
 
     private String reason;
