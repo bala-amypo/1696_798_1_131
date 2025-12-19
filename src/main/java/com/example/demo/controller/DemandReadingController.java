1caa8770bc310b3641c6aab0ad5b93e37e57e63c
bala-amypo <bala@amypo.in>
2025-12-19 16:05:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/DemandReadingController.java
+++ src/main/java/com/example/demo/controller/DemandReadingController.java
@@ -1,23 +1,9 @@
-package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.demo.service.DemandReadingService;
-
 @RestController
-@RequestMapping("/demand")
 public class DemandReadingController {
 
-    private final DemandReadingService demandReadingService;
+    private final DemandReadingService service;
 
-    public DemandReadingController(DemandReadingService demandReadingService) {
-        this.demandReadingService = demandReadingService;
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return demandReadingService.testService();
+    public DemandReadingController(DemandReadingService service) {
+        this.service = service;
     }
 }
