ca90b3e37cd8374f18d52af3dd621ab7981e2a5b
bala-amypo <bala@amypo.in>
2025-12-19 16:08:25 +0000

    User commit

--- src/main/java/com/example/demo/service/DemandReadingService.java
+++ src/main/java/com/example/demo/service/DemandReadingService.java
@@ -1,12 +1,5 @@
 package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class DemandReadingService {
-
-    public String testService() {
-        return "Service is working!";
-    }
-
+public interface DemandReadingService {
+    void save();
 }
