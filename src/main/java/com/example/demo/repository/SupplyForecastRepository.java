ecc329bb219f2f6b9ad09bcbd96731fa16e9deb7
bala-amypo <bala@amypo.in>
2025-12-19 05:20:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/SupplyForecastRepository.java
+++ src/main/java/com/example/demo/repository/SupplyForecastRepository.java
@@ -1,15 +1,5 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.SupplyForecast;
-
-public interface SupplyForecastRepository extends JpaRepository<SupplyForecast, Long> {
-
-    // Latest forecast
-    SupplyForecast findTopByOrderByForecastTimeDesc();
-}
-package com.example.demo.repository;
-
 import com.example.demo.entity.SupplyForecast;
 import org.springframework.data.jpa.repository.JpaRepository;
 
