d9c11a5cc1e8803711d7a8f6e3bb3b3e2d95347b
bala-amypo <bala@amypo.in>
2025-12-20 12:42:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -0,0 +1,43 @@
+package com.example.demo.security;
+
+import com.example.demo.entity.AppUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+@Component
+
+public class JwtTokenProvider {
+
+    private static final String SECRET_KEY = "secret-key-demo";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+    public String createToken(AppUser user) {
+
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("userId", user.getId())
+                .claim("role", user.getRoles().iterator().next().getName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
