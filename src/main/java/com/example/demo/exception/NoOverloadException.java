be88166447ab3094e9f8a736aa26eb6b61d6f415
bala-amypo <bala@amypo.in>
2025-12-25 15:33:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/NoOverloadException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class NoOverloadException extends RuntimeException {
+    public NoOverloadException(String message) {
+        super(message);
+    }
+}
