760d5bef76472a8cb4fd7034b64c6dba51380ef5
bala-amypo <bala@amypo.in>
2025-12-26 09:28:07 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 // ✅ Server configuration (you already had this)
                 .servers(List.of(
-                        new Server().url("9221.pro604cr.amypo.ai/")
+                        new Server().url("https://9221.pro604cr.amypo.ai/")
                 ))
 
                 // ✅ THIS enables the Authorize button
